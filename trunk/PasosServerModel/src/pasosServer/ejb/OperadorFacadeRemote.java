7595c33a512bd748d4e749a3214786a0b974805f
juanantonio.gargon86@gmail.com <juanantonio.gargon86@gmail.com@a7243c9c-ac54-eeee-72bf-91f3acd1cadb>
2012-01-25 12:30:22 +0000

    Modelo

---
+++ trunk/PasosServerModel/src/pasosServer/ejb/OperadorFacadeRemote.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasosServer.ejb;
+
+import java.util.List;
+import javax.ejb.Remote;
+import pasosServer.model.Operador;
+
+/**
+ *
+ * @author Juan Antonio
+ */
+@Remote
+public interface OperadorFacadeRemote {
+
+    void create(Operador operador);
+
+    void edit(Operador operador);
+
+    void remove(Operador operador);
+
+    Operador find(Object id);
+
+    List<Operador> findAll();
+
+    List<Operador> findRange(int[] range);
+
+    int count();
+    
+}
---
+++ trunk/PasosServerModel/src/pasosServer/ejb/ProtegidoFacadeRemote.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasosServer.ejb;
+
+import java.util.List;
+import javax.ejb.Remote;
+import pasosServer.model.Protegido;
+
+/**
+ *
+ * @author Juan Antonio
+ */
+@Remote
+public interface ProtegidoFacadeRemote {
+
+    void create(Protegido protegido);
+
+    void edit(Protegido protegido);
+
+    void remove(Protegido protegido);
+
+    Protegido find(Object id);
+
+    List<Protegido> findAll();
+
+    List<Protegido> findRange(int[] range);
+
+    int count();
+    
+}
