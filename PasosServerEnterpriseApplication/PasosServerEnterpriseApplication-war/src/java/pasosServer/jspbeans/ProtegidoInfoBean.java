13cbdeeb97013300647b5923863f694ee43e0fac
juanantonio.gargon86@gmail.com <juanantonio.gargon86@gmail.com@a7243c9c-ac54-eeee-72bf-91f3acd1cadb>
2012-01-25 15:39:25 +0000

    Mostrar datos usuario

---
+++ PasosServerEnterpriseApplication/PasosServerEnterpriseApplication-war/src/java/pasosServer/jspbeans/ProtegidoInfoBean.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasosServer.jspbeans;
+
+import pasosServer.model.Protegido;
+
+/**
+ *
+ * @author Juan Antonio
+ */
+public class ProtegidoInfoBean {
+    private Protegido protegido;
+    
+    public ProtegidoInfoBean(){
+        
+    }
+
+    /**
+     * @return the protegido
+     */
+    public Protegido getProtegido() {
+        return protegido;
+    }
+
+    /**
+     * @param protegido the protegido to set
+     */
+    public void setProtegido(Protegido protegido) {
+        this.protegido = protegido;
+    }
+
+}
