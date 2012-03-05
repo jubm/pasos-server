7b74e091bfd12208f2237509bf2c1d6598d002bf
rolimat@gmail.com <rolimat@gmail.com@a7243c9c-ac54-eeee-72bf-91f3acd1cadb>
2012-03-05 19:39:18 +0000

    Accesibilidad y cambio de temas listo

--- trunk/Android/src/org/inftel/pasos/modelo/Modelo.java
+++ trunk/Android/src/org/inftel/pasos/modelo/Modelo.java
@@ -9,92 +9,81 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-public class Modelo extends Observable implements Serializable{
+public class Modelo extends Observable implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Boolean notifVibracion;
 	private Boolean notifVoz;
-	private String tema;
+	private int tema;
 	private float tamTexto;
 	private SharedPreferences prefs;
 	private SharedPreferences.Editor editor;
-	
+
 	public Modelo(Context ctx) {
-		prefs = ctx.getSharedPreferences("prefs",
-				Activity.MODE_PRIVATE);
+		prefs = ctx.getSharedPreferences("prefs", Activity.MODE_PRIVATE);
 		editor = prefs.edit();
 
 		notifVibracion = prefs.getBoolean("vib", true);
 		notifVoz = prefs.getBoolean("voz", true);
-		tema = prefs.getString("tema", ctx.getString(R.string.tema1));
+		tema = prefs.getInt("tema", R.style.tema1);
 		tamTexto = prefs.getFloat("tam", 20);
 	}
 
-	
-	
 	public Boolean getNotifVibracion() {
 		return notifVibracion;
 	}
-
-
 
 	public float getTamTexto() {
 		return tamTexto;
 	}
 
-
-
 	public void setTamTexto(float tamTexto) {
 		this.tamTexto = tamTexto;
-		setChanged();
-		notifyObservers(this);	
+		editor.putFloat("tam", tamTexto);
+		if (editor.commit()) {
+			setChanged();
+			notifyObservers(this);
+		}
 	}
-
-
 
 	public void setNotifVibracion(Boolean notifVibracion) {
 		this.notifVibracion = notifVibracion;
 		editor.putBoolean("vib", notifVibracion);
-		editor.commit();
-		setChanged();
-		notifyObservers(this);	
+		if (editor.commit()) {
+			setChanged();
+			notifyObservers(this);
+		}
 	}
-
-
 
 	public Boolean getNotifVoz() {
 		return notifVoz;
 	}
 
-
-
 	public void setNotifVoz(Boolean notifVoz) {
 		this.notifVoz = notifVoz;
 		editor.putBoolean("voz", notifVoz);
-		editor.commit();
-		setChanged();
-		notifyObservers(this);	
+		if (editor.commit()) {
+			setChanged();
+			notifyObservers(this);
+		}
 	}
 
-
-
-	public String getTema() {
+	public int getTema() {
 		return tema;
 	}
 
-
-
-	public void setTema(String tema) {
+	public void setTema(int tema) {
 		this.tema = tema;
-		setChanged();
-		notifyObservers(this);	
+		editor.putInt("tema", tema);
+		if (editor.commit()) {
+			setChanged();
+			notifyObservers(this);
+		}
 	}
-
-
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-	
+
 }
